933d83d5e5dbed8054bc49c3568bf29e0dbb8ae5
viltti <ville.a.tiainen@aalto.fi>
2023-06-12 01:49:59 +0300

    modify main.py to work with gunicorn

--- main.py
+++ main.py
@@ -10,9 +10,15 @@ def create_app():
 
     return app
 
+app = create_app()
+
 if __name__ == "__main__":
     app = create_app()
+    app.run(debug=False, host='0.0.0.0', port=int(os.environ.get('PORT', 8080)))
+    
+    '''
     if os.getenv('FLASK_ENV') == 'development':
         app.run(debug=True)
     else:
         app.run(debug=True, host='0.0.0.0', port=int(os.environ.get('PORT', 8080)))
+    '''
